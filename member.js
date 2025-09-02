10e75162317f00d8d1dc52dd0579bba000fed3e9
Ethan Luna <135297643+ethanmluna@users.noreply.github.com>
2025-09-02 23:40:06 +0000

    This is the second commit!

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/skills/member.html',
+        scope: {
+            member: '='
+        }
+    };
+}
+
+export default skillsMember;
